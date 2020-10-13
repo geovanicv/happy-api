4cf36bc4a9c5738807c06a8861e25d627fd3e6ea
Geovani Cavalcante <geovani.scavalcante@gmail.com>
2020-10-13 17:59:15 -0300

    :sparkles: feat: add orphanage creation flow

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,41 @@
 import express from 'express';
 
+import {getRepository} from 'typeorm';
+
+import Orphanages from './models/Orphanages';
+
 import './database/connection';
 
 const app = express();
 
 app.use(express.json());
 
-app.get('/users', (request, response) => {
-  return response.send('hello world');
+app.post('/orphanages', async (request, response) => {
+  const { 
+    name, 
+    latitude,
+    longitude,
+    about,
+    instructions,
+    opening_hours,
+    open_on_weekends
+  } = request.body;
+
+  const orphanagesRepository = getRepository(Orphanages);
+
+  const orphanage = orphanagesRepository.create({
+    name,
+    latitude,
+    longitude,
+    about,
+    instructions,
+    opening_hours,
+    open_on_weekends
+  });
+
+  await orphanagesRepository.save(orphanage);
+  
+  return response.status(201).json(orphanage);
 });
 
 app.listen(3333);
