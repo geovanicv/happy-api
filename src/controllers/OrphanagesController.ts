535403c0f51494312faf5cff8f28c3695ff74fe6
Geovani Cavalcante <geovani.scavalcante@gmail.com>
2020-10-14 01:43:33 -0300

    :sparkles: feat: validate orphanages data and update error handler to show the validation errors of yup

--- src/controllers/OrphanagesController.ts
+++ src/controllers/OrphanagesController.ts
@@ -2,6 +2,8 @@ import {Request, Response} from 'express'
 
 import {getRepository} from 'typeorm';
 
+import * as Yup from 'yup';
+
 import orphanageView from '../views/orphanages-view';
 import Orphanages from '../models/Orphanages';
 import orphanagesView from '../views/orphanages-view';
@@ -49,7 +51,7 @@ export default {
   
     const orphanagesRepository = getRepository(Orphanages);
   
-    const orphanage = orphanagesRepository.create({
+    const data = {
       name,
       latitude,
       longitude,
@@ -58,7 +60,27 @@ export default {
       opening_hours,
       open_on_weekends,
       images
+    }
+
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      latitude: Yup.number().required(),
+      longitude: Yup.number().required(),
+      about: Yup.string().required().max(300),
+      instructions: Yup.string().required(),
+      opening_hours: Yup.string().required(),
+      open_on_weekends: Yup.boolean().required(),
+      images: Yup.array(
+        Yup.object().shape({
+          path: Yup.string().required()
+      }))
     });
+
+    await schema.validate(data, {
+      abortEarly: false, //retorna todos os erros ao mesmo tempo
+    })
+
+    const orphanage = orphanagesRepository.create(data);
   
     await orphanagesRepository.save(orphanage);
     
